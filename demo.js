02d531b7df10a84c03ad56d77b462d6fe8db81c7
Kiss-Armand <nomminecraftserver@gmail.com>
2019-04-25 17:27:45 +0200

    The demo default ids have been mofifyed.

--- demo.js
+++ demo.js
@@ -3,11 +3,11 @@ const Server = require('./server');
 let demo = new engine.Engine("Game", "black");
 demo.init();
 demo.onstart_start();
-demo.addActor("kecske",10,10,"yellow",10,10);
-demo.onkey('w', demo.moveActor("kecske",true,0,10,true));
-demo.onkey('a', demo.moveActor("kecske",true,-10,0,true));
-demo.onkey('s', demo.moveActor("kecske",true,0,-10,true));
-demo.onkey('d', demo.moveActor("kecske",true,10,0,true));
+demo.addActor("player",10,10,"green",10,10);
+demo.onkey('w', demo.moveActor("player",true,0,10,true));
+demo.onkey('a', demo.moveActor("player",true,-10,0,true));
+demo.onkey('s', demo.moveActor("player",true,0,-10,true));
+demo.onkey('d', demo.moveActor("player",true,10,0,true));
 demo.onstart_end();
 demo.start();
 
