f6bb35b50ae966a1fad1dd7af92652b327ba77e5
Kiss-Armand <nomminecraftserver@gmail.com>
2019-04-25 18:22:01 +0200

    🕮 Added comments

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const http = require('http');
+const http = require('http'); //Imports the built in http packages
 
 class Server {
     constructor(code, port) {
-        this.code = code;
-        this.port = port;
+        this.code = code; //Saves the html code of the engine
+        this.port = port; //Saves the port
     }
     start() {
-        let code = this.code;
-        let port = this.port;
+        let code = this.code; //Saves the this.code var to code
+        let port = this.port; //Saves the this.port var to port
         http.createServer(function(req,resp){
             resp.writeHead(200, { "Content-Type" : "text/html"});  
-            resp.write(code);
-            resp.end();
-        }).listen(port);
+            resp.write(code); //Reads the code variable and sends it
+            resp.end(); //Ends the requiest
+        }).listen(port); //Reads the port variable
     }
 }
 
