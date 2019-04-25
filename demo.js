3e14e9e8fdc3f7a232d2c0a09bde6efe0c33c37e
Kiss-Armand <nomminecraftserver@gmail.com>
2019-04-25 17:56:58 +0200

    🕮 Added comments

--- demo.js
+++ demo.js
@@ -3,13 +3,13 @@ const Server = require('./server'); //Import server.js
 
 let demo = new engine.Engine("Game", "black"); //Create a new canvas with back color and set the window title to "Game"
 demo.init(); //Initialize the engine
-demo.onstart_start(); //On start {
+demo.onstart_start(); //On start beggining
 demo.addActor("player",10,10,"green",10,10); // Create a new player by id "player" with 10x10 size, color green and x=10,y=10
 demo.onkey('w', demo.moveActor("player",true,0,10,true)); //Lisen to press w and then move the player by 10 on y
 demo.onkey('a', demo.moveActor("player",true,-10,0,true)); //Lisen to press a and then move the player by -10 on x
 demo.onkey('s', demo.moveActor("player",true,0,-10,true)); //Lisen to press s and then move the player by -10 on y
 demo.onkey('d', demo.moveActor("player",true,10,0,true)); //Lisen to press d and then move the player by 10 on x
-demo.onstart_end(); //On start }
+demo.onstart_end(); //On start end
 demo.start(); //Start the game
 
 let demoserver = new Server.Server(demo.render(),8888); // Setup an http server on port 8888
