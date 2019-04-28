f95db6ffadd4cedd61bb82edab159f16d9ec2275
Armand-Kiss <nomminecraftserver@gmail.com>
2019-04-28 10:24:08 +0200

    Server is running Message added.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ class Server {
     start() {
         let code = this.code; //Saves the this.code var to code
         let port = this.port; //Saves the this.port var to port
+        console.log("Server is running on http://localhost:" + port) //Returns the port of the server
         http.createServer(function(req,resp){
             resp.writeHead(200, { "Content-Type" : "text/html"});  
             resp.write(code); //Reads the code variable and sends it
