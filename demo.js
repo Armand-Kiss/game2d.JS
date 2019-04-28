8ac692974f52ab2252bc87f03f2fb07d7a3bef4f
Armand-Kiss <nomminecraftserver@gmail.com>
2019-04-28 10:37:57 +0200

    Sample music added.

--- demo.js
+++ demo.js
@@ -3,7 +3,8 @@ const Server = require('./server'); //Import server.js
 
 let demo = new engine.Engine("Game", "black"); //Create a new canvas with back color and set the window title to "Game"
 demo.init(); //Initialize the engine
-demo.onstart_start(); //On start beggining
+demo.onstart_start(); //On start beggining ON FIREFOX YOU HAVE TO ENABLE PLAY MUSIC AUTOMATICALLY!
+demo.playSound("samplemusic", "http:\/\/www.noiseaddicts.com/samples_1w72b820/3732.mp3");
 demo.addActor("player",10,10,"green",10,10); // Create a new player by id "player" with 10x10 size, color green and x=10,y=10
 demo.onkey('w', demo.moveActor("player",true,0,10,true)); //Lisen to press w and then move the player by 10 on y
 demo.onkey('a', demo.moveActor("player",true,-10,0,true)); //Lisen to press a and then move the player by -10 on x
