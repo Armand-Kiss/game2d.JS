7a545910cd4a360079797687f0691e7065c238e8
Armand-Kiss <nomminecraftserver@gmail.com>
2019-04-29 20:04:46 +0200

    Client Side Obfuscation

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 const engine = require('./engine'); //Import engine.js
 const Server = require('./server'); //Import server.js
 
-let demo = new engine.Engine("Game", "black"); //Create a new canvas with back color and set the window title to "Game"
+let demo = new engine.Engine("Game", "black",true); //Create a new canvas with back color and set the window title to "Game"
 demo.init(); //Initialize the engine
 demo.onstart_start(); //On start beggining ON FIREFOX YOU HAVE TO ENABLE PLAY MUSIC AUTOMATICALLY!
-demo.playSound("samplemusic", "http:\/\/www.noiseaddicts.com/samples_1w72b820/3732.mp3");
+//demo.playSound("samplemusic", "http:\/\/www.noiseaddicts.com/samples_1w72b820/3732.mp3");
 demo.addActor("player",10,10,"green",10,10); // Create a new player by id "player" with 10x10 size, color green and x=10,y=10
 demo.onkey('w', demo.moveActor("player",true,0,10,true)); //Lisen to press w and then move the player by 10 on y
 demo.onkey('a', demo.moveActor("player",true,-10,0,true)); //Lisen to press a and then move the player by -10 on x
@@ -13,5 +13,5 @@ demo.onkey('d', demo.moveActor("player",true,10,0,true)); //Lisen to press d and
 demo.onstart_end(); //On start end
 demo.start(); //Start the game
 
-let demoserver = new Server.Server(demo.render(),8888); // Setup an http server on port 8888
+let demoserver = new Server.Server(demo.export(true),8888); // Setup an http server on port 8888
 demoserver.start(); //Start the http server
--- engine.js
+++ engine.js
@@ -1,15 +1,96 @@
 const fm = require('./fm'); //Import "FileManager"
+const jsflib = require("./jsfuck.js");
 class Engine {
-  constructor(title, backgroundColor) {
+  constructor(title, backgroundColor, doobfuscate) {
     this.actors = []; //Creates an empty array
+    this.obfuscate = false;
+    if (doobfuscate == null) {
+      this.obfuscate = false;
+    }
+    if (doobfuscate == true) {
+      this.obfuscate = true;
+    }
     this.title = title; //Saves the title
     this.backgroundColor = backgroundColor; //Saves the background color
   }
   init() { //Initialize the Engine
-    this.out = "<!DOCTYPE html>\n \
+    if (this.obfuscate == true) {
+      this.out = "<!DOCTYPE html>\n \
     <html>\n \
     <head>\n \
     <title>" + this.title + "</title>\n \
+    <meta charset=\"utf-8\"> \n \
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n \
+    <style>\n \
+    canvas {\n \
+        border:1px solid #d3d3d3;\n \
+        background-color: " + this.backgroundColor + ";\n \
+    }\n \
+    </style>\n \
+    </head>\n \
+    <body onresize=\"resizew()\"> \n \
+    <script>\n \
+    let gamecanvas = document.getElementsByTagName(\"canvas\")[0]; \n \
+    \n \
+    " + "eval(" + jsflib.JSFuck.encode("function resizew() {\n \
+      let gamecanvas = document.getElementsByTagName(\"canvas\")[0]; \n \
+      var w = window.innerWidth;\n \
+      var h = window.innerHeight;\n \
+      gamecanvas.width = w - 21;\n \
+      gamecanvas.height = h - 30;\n \
+      updateall(); \n \
+    } \n ",false) + ");" + 
+    "var myGameArea = {\n \
+        canvas : document.createElement(\"canvas\"),\n \
+        start : function() {\n \
+            var w = window.innerWidth;\n \
+            var h = window.innerHeight;\n \
+            this.canvas.width = w - 21;\n \
+            this.canvas.height = h - 30;\n \
+            this.context = this.canvas.getContext(\"2d\");\n \
+            document.body.insertBefore(this.canvas, document.body.childNodes[0]);\n \
+        }\n \
+    }\n \
+     \n \
+    function addEvent(element, eventName, callback) { \n \
+        if (element.addEventListener) { \n \
+            element.addEventListener(eventName, callback, false); \n \
+        } else if (element.attachEvent) { \n \
+            element.attachEvent(\"on\" + eventName, callback); \n \
+        } else { \n \
+            element[\"on\" + eventName] = callback; \n \
+        } \n \
+    } \n \
+    " + "eval(" + jsflib.JSFuck.encode("function component(width, height, color, x, y) { \n \
+      this.width = width; \n \
+      this.height = height; \n \
+      this.speedX = 0; \n \
+      this.speedY = 0; \n \
+      this.x = x; \n \
+      this.y = y;     \n \
+      this.update = function() { \n \
+          \n \
+          ctx = myGameArea.context; \n \
+          ctx.fillStyle = color; \n \
+          ctx.fillRect(this.x, this.y, this.width, this.height); \n \
+      } \n \
+      this.newPos = function(relative,xx,yy) { \n \
+          if (relative) {\n\
+          this.x += xx; \n \
+          this.y += yy;         \n \
+          } else { \n \
+          this.x = xx; \n \
+          this.y = yy;\n \
+          } \n \
+      }     \n \
+  } \n ", false) +
+    ");\n ";
+    } else {
+      this.out = "<!DOCTYPE html>\n \
+    <html>\n \
+    <head>\n \
+    <title>" + this.title + "</title>\n \
+    <meta charset=\"utf-8\"> \n \
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n \
     <style>\n \
     canvas {\n \
@@ -75,17 +156,21 @@ class Engine {
       }     \n \
   } \n \
     \n ";
+    }
   }
   onstart_start() { //onstart begining
     this.out += "function startGame() {\n       myGameArea.start();\n ";
   }
   addCode(jscode) { //Adds browser-side javascript code
+    if (this.obfuscate) {
+      this.out += "eval(" + jsflib.JSFuck.encode(jscode,false) + ");";
+    } else {
     this.out += jscode;
+    }
   }
   moveActor(acotrname,relative,x,y, asreturn) { //moves the actor
     if (!asreturn) {
-    this.out += acotrname + ".newPos(" + relative + ',' + x + ',' + -y + ");\n";
-    this.out += "updateall(); \n";
+    this.out += acotrname + ".newPos(" + relative + ',' + x + ',' + -y + ");updateall();\n";
     } else {
       let a = acotrname + ".newPos(" + relative + ',' + x + ',' + -y + ");\n";
       a += "updateall(); \n";
@@ -94,7 +179,7 @@ class Engine {
   }
   onstart_end() { //ends the onstart
     this.out += "function updateall() {\n"
-    this.out += "myGameArea.start();"
+    this.out += "eval(" + jsflib.JSFuck.encode("myGameArea.start();",false) + ");"
     let arr = this.actors;
     let temp = "";
     for (let i = 0; i < this.actors.length; i++) {
@@ -109,11 +194,19 @@ class Engine {
     this.out += "\nconsole.log(\"" + text + "\");\n";
   }
   logkeys() { //Logs the keys that are pressed
+    if (this.obfuscate) {
+      this.out += jsflib.JSFuck.encode("addEvent(document, \"keypress\", function (e) { \n \
+        e = e || window.event; \n \
+        console.log(e.key); \n \
+      }); \n \
+      ",true);
+    } else {
     this.out += "addEvent(document, \"keypress\", function (e) { \n \
       e = e || window.event; \n \
       console.log(e.key); \n \
     }); \n \
     ";
+  }
   }
   pasueSound(id) { //Pauses a sound
   this.out += id + ".pause();";
@@ -146,6 +239,14 @@ class Engine {
     return -number;
   }
   onkey(letter, code) { //On key press
+    if (this.obfuscate) {
+      this.out += "addEvent(document, \"keypress\", function (e) { \n \
+        e = e || window.event; \n \
+        if (\"" + letter + "\" == e.key) \n \
+        {" + '\n' + "eval(" + jsflib.JSFuck.encode(code,false) + ");" + "};\n \
+      }); \n \
+      ";
+    } else {
     this.out += "addEvent(document, \"keypress\", function (e) { \n \
       e = e || window.event; \n \
       if (\"" + letter + "\" == e.key) \n \
@@ -153,20 +254,40 @@ class Engine {
     }); \n \
     ";
   }
+  }
   addActor(id,width,height,color, x,y) { //Adds an actor
     this.actors.push(id); //Pushes the actor to the array
+    if (this.obfuscate) {
+      this.out += "eval(" + jsflib.JSFuck.encode("       var " + id + ";\n \
+        " + id + " = new component(" + width + ", " + height + ", \"" + color + "\", " + x + ", " + y + ");" + " \n",false) + ");";
+    }
     this.out += 
     "       var " + id + ";\n \
         " + id + " = new component(" + width + ", " + height + ", \"" + color + "\", " + x + ", " + y + ");" + " \n";
         this.out += "\n " + id + ".update();"; //generates the code
       }
   start() { //Starts the game, and the Engine
+    if (this.obfuscate) {
+      this.out += "\n         eval("+ jsflib.JSFuck.encode("startGame();", false) + ");";
+    } else {
     this.out += "\n         startGame(); \n";
+    }
   }
-  render() { //Finishes the engine script, and returns it in html
-    this.out += "</script>\n "
-    return this.out;
+  export(antidebug) { //Finishes the engine script, and returns it in html
+    if (antidebug == null) {
+      this.out += "</script>\n ";
+      return this.out;
+    }
+    if (antidebug) {
+      this.out += jsflib.JSFuck.encode("function ___(){var __a=eval(\"performance.now();\");eval(\"debugger;\");var __b=eval(\"performance.now();\");__b-__a>1e3&&location.reload(),setTimeout(___,200)}___();throw(\"Please close the developer tools!\");", true);
+      this.out += "</script>\n ";
+      return this.out;
+    }
+
   }
 }
+
+
+
 
 exports.Engine = Engine;
